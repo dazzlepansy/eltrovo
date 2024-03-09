c3381b30b5eb177f9609cd8790ab30df150df274
St John Karp <contact@stjohnkarp.net>
2024-03-09 14:05:29 -0500

    Implement a folder picker
    Replaced the hard-coded folder path with a folder picker UI.
    Imported the old code from the command-line project and hooked
    it up to the UI. Results are still output to the console.

--- EltrovoUI/ViewModels/MainWindowViewModel.cs
+++ EltrovoUI/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,48 @@
-﻿namespace EltrovoUI.ViewModels;
+﻿﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
 
-public class MainWindowViewModel : ViewModelBase
+namespace EltrovoUI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    [RelayCommand]
+    private async Task OpenFolder(CancellationToken token)
+    {
+        var folder = await DoOpenFilePickerAsync();
+
+        var path = folder?.TryGetLocalPath();
+            
+        if (path is not null) {
+            HashingOperations.GetSsdeepHashes(path);
+        }
+
+        return;
+    }
+
+    private async Task<IStorageFolder?> DoOpenFilePickerAsync()
+    {
+        // For learning purposes, we opted to directly get the reference
+        // for StorageProvider APIs here inside the ViewModel. 
+
+        // For your real-world apps, you should follow the MVVM principles
+        // by making service classes and locating them with DI/IoC.
+
+        // See IoCFileOps project for an example of how to accomplish this.
+        if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
+            desktop.MainWindow?.StorageProvider is not { } provider)
+            throw new NullReferenceException("Missing StorageProvider instance.");
+
+        var folders = await provider.OpenFolderPickerAsync(new FolderPickerOpenOptions()
+        {
+            Title = "Open Folder",
+            AllowMultiple = false
+        });
+
+        return folders?.Count >= 1 ? folders[0] : null;
+    }
 }
